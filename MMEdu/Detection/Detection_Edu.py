a91be9d66a112c2364e36d185012a7e4a2c567bf
phoneix-s <59813103+phoneix-s@users.noreply.github.com>
2022-09-07 13:11:09 +0800

    Update Detection_Edu.py

--- MMEdu/Detection/Detection_Edu.py
+++ MMEdu/Detection/Detection_Edu.py
@@ -1,130 +1,141 @@
 import os
-import json
-from tkinter import E
 import mmcv
 import time
 import torch
 from mmcv import Config
-from mmdet.apis import inference_detector, init_detector, show_result_pyplot, train_detector
-from mmdet.models import build_detector
-from mmdet.datasets import build_dataset
+from mmcls.apis import inference_model, init_model, show_result_pyplot, train_model, set_random_seed, single_gpu_test
+from mmcls.models import build_classifier
+from mmcls.datasets import  build_dataloader,build_dataset
 from mmcv.runner import load_checkpoint
 from tqdm import tqdm
-import warnings
-warnings.filterwarnings("ignore")
+import numpy as np
 
-class MMDetection:
-    def __init__(self, 
-        backbone='FasterRCNN',
-        num_classes=-1,
-        dataset_path = None
-        ):
+
+class MMClassification:
+    def sota():
+        pypath = os.path.abspath(__file__)
+        father = os.path.dirname(pypath)
+        models = os.path.join(father, 'models')
+        sota_model = []
+        for i in os.listdir(models):
+            if i[0] != '_':
+                sota_model.append(i)
+        return sota_model
+
+    def __init__(
+            self,
+            backbone='LeNet',
+            num_classes=-1,
+            dataset_path='../dataset/cls/hand_gray',
+            # dataset_type = 'ImageNet'
+    ):
 
         # 获取外部运行py的绝对路径
         self.cwd = os.path.dirname(os.getcwd())
         # 获取当前文件的绝对路径
         self.file_dirname = os.path.dirname(os.path.abspath(__file__))
+        self.save_fold = None
 
-        self.config = os.path.join(self.file_dirname, 'models', 'FasterRCNN/FasterRCNN.py')
-        self.checkpoint = os.path.join(self.file_dirname, 'models', '/FasterRCNN/FasterRCNN.pth')
-        
+        self.config = os.path.join(self.file_dirname, 'models', 'LeNet/LeNet.py')
+        self.checkpoint = os.path.join(self.file_dirname, 'models', 'LeNet/LeNet.pth')
+
         self.backbone = backbone
         backbone_path = os.path.join(self.file_dirname, 'models', self.backbone)
         ckpt_cfg_list = list(os.listdir(backbone_path))
         for item in ckpt_cfg_list:
-            if item[-1] == 'y':
+            if item[-1] == 'y' and item[0] != '_':  #pip修改1
                 self.config = os.path.join(backbone_path, item)
             elif item[-1] == 'h':
                 self.checkpoint = os.path.join(backbone_path, item)
             else:
-                # print("Warning!!! There is an unrecognized file in the backbone folder.")
+                #     print("Warning!!! There is an unrecognized file in the backbone folder.")
                 pass
 
         self.cfg = Config.fromfile(self.config)
-
         self.dataset_path = dataset_path
         self.lr = None
         self.backbonedict = {
-            "FasterRCNN": os.path.join(self.file_dirname, 'models', 'FasterRCNN/FasterRCNN.py'),
-            "Yolov3": os.path.join(self.file_dirname, 'models', 'Yolov3/Yolov3.py'),
-            "Mask_RCNN": os.path.join(self.file_dirname, 'models', 'Mask_RCNN/Mask_RCNN.py'),
-            "SSD_Lite": os.path.join(self.file_dirname, 'models', 'SSD_Lite/SSD_Lite.py'),
+            'MobileNet': os.path.join(self.file_dirname, 'models', 'MobileNet/MobileNet.py'),
+            'ResNet18': os.path.join(self.file_dirname, 'models', 'ResNet18/ResNet18.py'),
+            'ResNet50': os.path.join(self.file_dirname, 'models', 'ResNet50/ResNet50.py'),
+            'LeNet': os.path.join(self.file_dirname, 'models', 'LeNet/LeNet.py'),
             # 下略
         }
+
         self.num_classes = num_classes
         self.chinese_res = None
 
-
-    def train(self, random_seed=0, save_fold=None, distributed=False, validate=True,
-              metric='bbox', optimizer="SGD", epochs=100, lr=0.001, weight_decay=0.001, Frozen_stages=1,
-              checkpoint = None, encoding = 'utf-8'):
-        
-        # 加载网络模型的配置文件
+    def train(self, random_seed=0, save_fold=None, distributed=False, validate=True, device="cpu",
+              metric='accuracy', save_best='auto', optimizer="SGD", epochs=100, lr=0.01, weight_decay=0.001,
+              checkpoint=None):
+        set_random_seed(seed=random_seed)
+        # 获取config信息
         self.cfg = Config.fromfile(self.backbonedict[self.backbone])
-        #print("进行了cfg的切换")
 
         # 如果外部不指定save_fold
         if not self.save_fold:
             # 如果外部也没有传入save_fold，我们使用默认路径
             if not save_fold:
-                self.save_fold = os.path.join(self.cwd, 'checkpoints/det_model')
+                self.save_fold = os.path.join(self.cwd, 'checkpoints/cls_model')
             # 如果外部传入save_fold，我们使用传入值
             else:
                 self.save_fold = save_fold
 
-        self.cfg.model.backbone.frozen_stages = Frozen_stages
-
-        # for two-stage
-        if self.backbone == 'FasterRCNN' or 'Mask_RCNN':
-            self.cfg.model.roi_head.bbox_head.num_classes = self.num_classes
-        else:
-            self.cfg.model.bbox_head.num_classes = self.num_classes
+        if self.num_classes != -1:
+            if 'num_classes' in self.cfg.model.backbone.keys():
+                self.cfg.model.backbone.num_classes = self.num_classes
+            else:
+                self.cfg.model.head.num_classes = self.num_classes
 
         self.load_dataset(self.dataset_path)
-        # 添加需要进行检测的类名
-        self.cfg.classes = self.get_classes(self.cfg.data.train.ann_file, encoding)
-        # 分别为训练、测试、验证添加类名
-        self.cfg.data.train.classes = self.cfg.classes
-        self.cfg.data.test.classes = self.cfg.classes
-        self.cfg.data.val.classes = self.cfg.classes
 
         # 进行
         self.cfg.work_dir = self.save_fold
         # 创建工作目录
         mmcv.mkdir_or_exist(os.path.abspath(self.cfg.work_dir))
         # 创建分类器
-        datasets = [build_dataset(self.cfg.data.train)]
-        model = build_detector(self.cfg.model, train_cfg=self.cfg.get('train_cfg'), test_cfg=self.cfg.get('test_cfg'))
+        model = build_classifier(self.cfg.model)
         if not checkpoint:
             model.init_weights()
         else:
             load_checkpoint(model, checkpoint, map_location=torch.device('cpu'))
+            # model = init_model(self.cfg, checkpoint)
 
-        model.CLASSES = self.cfg.classes
-        # 根据输入参数更新config文件
-        self.cfg.optimizer.lr = lr  # 学习率
-        self.cfg.optimizer.type = optimizer  # 优化器
+        datasets = [build_dataset(self.cfg.data.train)]
+        # 添加类别属性以方便可视化
+        model.CLASSES = datasets[0].CLASSES
+
+        n_class = len(model.CLASSES)
+        if n_class <= 5:
+            self.cfg.evaluation.metric_options = {'topk': (1,)}
+        else:
+            self.cfg.evaluation.metric_options = {'topk': (5,)}
         if optimizer == 'Adam':
             self.cfg.optimizer = dict(type='Adam', lr=lr,betas=(0.9, 0.999),eps=1e-8, weight_decay=0.0001)
         elif optimizer == 'Adagrad':
             self.cfg.optimizer = dict(type='Adagrad',lr=lr, lr_decay=0)
+        # 根据输入参数更新config文件
+        self.cfg.optimizer.lr = lr  # 学习率
+        self.cfg.optimizer.type = optimizer  # 优化器
         self.cfg.optimizer.weight_decay = weight_decay  # 优化器的衰减权重
         self.cfg.evaluation.metric = metric  # 验证指标
+        self.cfg.evaluation.save_best = save_best  #
         self.cfg.runner.max_epochs = epochs  # 最大的训练轮次
 
-        # 设置每 5 个训练批次输出一次日志
-        # self.cfg.log_config.interval = 1
+        # 设置每 10 个训练批次输出一次日志
+        self.cfg.log_config.interval = 10
         self.cfg.gpu_ids = range(1)
 
         self.cfg.seed = random_seed
 
-        train_detector(
+        train_model(
             model,
             datasets,
             self.cfg,
             distributed=distributed,
             validate=validate,
             timestamp=time.strftime('%Y%m%d_%H%M%S', time.localtime()),
+            device=device,
             meta=dict()
         )
 
@@ -134,100 +145,137 @@ class MMDetection:
         return self.chinese_res
 
     def inference(self, device='cpu',
-                  pretrain_model=None,
-                  is_trained=True,
-                  infer_data=None, 
-                  show=True,  
-                  rpn_threshold=0.5, 
-                  rcnn_threshold=0.3,
-                  save_fold='det_result',
-                  encoding='utf-8'
-        ):
-        if not pretrain_model:
-            pretrain_model = os.path.join(self.cwd, 'checkpoints/det_model/plate/latest.pth')
+                  checkpoint=None,
+                  image=None,
+                  show=True,
+                  class_path="../dataset/classes/cls_classes.txt",
+                  save_fold='cls_result'
+                  ):
+
+        if not checkpoint:
+            checkpoint = os.path.join(self.cwd, 'checkpoints/cls_model/hand_gray/latest.pth')
 
         print("========= begin inference ==========")
+        classed_name = self.get_class(class_path)
+        self.num_classes = len(classed_name)
 
         if self.num_classes != -1:
-            self.cfg.model.roi_head.bbox_head.num_classes = self.num_classes
+            if 'num_classes' in self.cfg.model.backbone.keys():
+                self.cfg.model.backbone.num_classes = self.num_classes
+            else:
+                self.cfg.model.head.num_classes = self.num_classes
 
-        checkpoint = self.checkpoint
-        if is_trained:
-            # 加载数据集及配置文件的路径
-            checkpoint = pretrain_model
-            self.load_dataset(self.dataset_path)
-            # 修正检测的目标
-            self.cfg.classes = self.get_classes(self.cfg.data.train.ann_file, encoding)
-            self.cfg.data.train.classes = self.cfg.classes
-            self.cfg.data.test.classes = self.cfg.classes
-            self.cfg.data.val.classes = self.cfg.classes
-            self.cfg.model.roi_head.bbox_head.num_classes = len(self.cfg.classes)
-            model = init_detector(self.cfg, checkpoint, device=device)
-            model.CLASSES = self.cfg.classes
-        else:
-            model = init_detector(self.cfg, checkpoint, device=device)
-        model.test_cfg.rpn.nms.iou_threshold = 1 - rpn_threshold
-        model.test_cfg.rcnn.nms.iou_threshold = 1 - rcnn_threshold
+        checkpoint = os.path.abspath(checkpoint) # pip修改2
 
+        
         results = []
-        if(infer_data[-1]!='/'):
-            img_array = mmcv.imread(infer_data)
-            result = inference_detector(model, img_array) # 此处的model和外面的无关,纯局部变量
-            if show == True:
-                show_result_pyplot(model, infer_data, result)
+        if (image[-1] != '/'):
+            if self.backbone != "LeNet":
+                model = init_model(self.cfg, checkpoint, device=device)
+                model.CLASSES = classed_name
+                img_array = mmcv.imread(image, flag='color')
+                result = inference_model(model, img_array)  # 此处的model和外面的无关,纯局部变量
+            else: 
+                # build the dataloader
+                dataset_path = os.getcwd()
+                f = open("test.txt",'w')
+                f.write(image)
+                f.write(" 1")
+                f.write('\n')
+                f.write("no.png 0")
+                f.close()
+                if not os.path.exists("test_set"):
+                    os.mkdir('test_set')
+                import shutil
+                if not os.path.exists(image):
+                    shutil.copyfile(image, os.path.join("test_set", image))
+                shutil.copyfile(image, os.path.join("test_set", "no.png"))
+                self.cfg.data.test.data_prefix = os.path.join(dataset_path,'test_set')
+                self.cfg.data.test.ann_file = os.path.join(dataset_path,'test.txt')
+                self.cfg.data.test.classes = os.path.abspath(class_path)
+
+                dataset = build_dataset(self.cfg.data.test)
+                # the extra round_up data will be removed during gpu/cpu collect
+                data_loader = build_dataloader(
+                    dataset,
+                    samples_per_gpu=self.cfg.data.samples_per_gpu,
+                    workers_per_gpu=self.cfg.data.workers_per_gpu,
+                    shuffle=False,
+                    round_up=True)
+                model = build_classifier(self.cfg.model)
+                checkpoint = load_checkpoint(model, checkpoint)
+                result = single_gpu_test(model,data_loader )
+
+                f = open(class_path, "r")
+                ff = f.readlines()
+                f.close()
+                # print("\n",np.argmax(result[0]), ff[np.argmax(result[0])][-1:])
+                pred_class = ff[np.argmax(result[0])] if ff[np.argmax(result[0])][-1:] != "\n" else ff[np.argmax(result[0])][:-1]
+                result = {
+                    'pred_label':np.argmax(result[0]),
+                    'pred_score':result[0][np.argmax(result[0])],
+                    'pred_class':pred_class,
+                }
+            model.show_result(image, result, show=show, out_file=os.path.join(save_fold, os.path.split(image)[1]))
             chinese_res = []
-            for j in range(result[0].shape[0]):
-                tmp = {}
-                tmp['置信度'] =  result[0][j][4]
-                tmp['坐标'] = {"x":int(result[0][j][0]),"y":int(result[0][j][1]),'w':int(result[0][j][2]),'h':int(result[0][j][3])}
-                # img.append(tmp)
-                chinese_res.append(tmp)
+            tmp = {}
+            tmp['标签'] = result['pred_label']
+            tmp['置信度'] = result['pred_score']
+            tmp['预测结果'] = result['pred_class']
+            # img.append(tmp)
+            chinese_res.append(tmp)
             # print(chinese_res)
             self.chinese_res = chinese_res
+            print("========= finish inference ==========")
             return result
         else:
-            img_dir = infer_data
+            model = init_model(self.cfg, checkpoint, device=device)
+            model.CLASSES = classed_name
+            img_dir = image
             mmcv.mkdir_or_exist(os.path.abspath(save_fold))
             chinese_results = []
-            for i,img in enumerate(tqdm(os.listdir(img_dir))):
-                result = inference_detector(model,img_dir+ img) # 此处的model和外面的无关,纯局部变量
-                model.show_result(img_dir+img,result, out_file=os.path.join(save_fold,img))
+            for i, img in enumerate(tqdm(os.listdir(img_dir))):
+                result = inference_model(model, img_dir + img)  # 此处的model和外面的无关,纯局部变量
+                model.show_result(img_dir + img, result, out_file=os.path.join(save_fold, os.path.split(img)[1]))
                 chinese_res = []
-                for j in range(result[0].shape[0]):
-                    tmp = {}
-                    tmp['置信度'] =  result[0][j][4]
-                    tmp['坐标'] = {"x":int(result[0][j][0]),"y":int(result[0][j][1]),'w':int(result[0][j][2]),'h':int(result[0][j][3])}
-                    # img.append(tmp)
-                    chinese_res.append(tmp)
+                chinese_res = []
+                tmp = {}
+                tmp['标签'] = result['pred_label']
+                tmp['置信度'] = result['pred_score']
+                tmp['预测结果'] = result['pred_class']
+                # img.append(tmp)
+                chinese_res.append(tmp)
                 chinese_results.append(chinese_res)
                 results.append(result)
             self.chinese_res = chinese_results
+
+        print("========= finish inference ==========")
+
         return results
 
-
-    def load_dataset(self, path, dataset_type='coco'):
+    def load_dataset(self, path):
         self.dataset_path = path
-        #数据集修正为coco格式
-        if self.backbone == 'SSD_Lite':
-            self.cfg.data.train.dataset.img_prefix = os.path.join(self.dataset_path, 'images/train/')
-            self.cfg.data.train.dataset.ann_file = os.path.join(self.dataset_path, 'annotations/train.json')
-        else:
-            self.cfg.data.train.img_prefix = os.path.join(self.dataset_path, 'images/train/')
-            self.cfg.data.train.ann_file = os.path.join(self.dataset_path, 'annotations/train.json')
 
-        self.cfg.data.val.img_prefix = os.path.join(self.dataset_path, 'images/test/')
-        self.cfg.data.val.ann_file = os.path.join(self.dataset_path, 'annotations/valid.json')
+        self.cfg.img_norm_cfg = dict(
+            mean=[124.508, 116.050, 106.438],
+            std=[58.577, 57.310, 57.437],
+            to_rgb=True
+        )
 
-        self.cfg.data.test.img_prefix = os.path.join(self.dataset_path, 'images/test/')
-        self.cfg.data.test.ann_file = os.path.join(self.dataset_path, 'annotations/valid.json')
+        self.cfg.data.train.data_prefix = os.path.join(self.dataset_path, 'training_set')
+        self.cfg.data.train.classes = os.path.join(self.dataset_path, 'classes.txt')
 
+        self.cfg.data.val.data_prefix = os.path.join(self.dataset_path, 'val_set')
+        self.cfg.data.val.ann_file = os.path.join(self.dataset_path, 'val.txt')
+        self.cfg.data.val.classes = os.path.join(self.dataset_path, 'classes.txt')
 
-    def get_classes(self, annotation_file, encoding = 'utf-8'):
-        classes = ()
-        with open(annotation_file, 'r', encoding = encoding) as f:
-            dataset = json.load(f)
-            # categories = dataset["categories"]
-            if 'categories' in dataset:
-                for cat in dataset['categories']:
-                    classes = classes + (cat['name'],)
+        self.cfg.data.test.data_prefix = os.path.join(self.dataset_path, 'test_set')
+        self.cfg.data.test.ann_file = os.path.join(self.dataset_path, 'test.txt')
+        self.cfg.data.test.classes = os.path.join(self.dataset_path, 'classes.txt')
+
+    def get_class(self, class_path):
+        classes = []
+        with open(class_path, 'r') as f:
+            for name in f:
+                classes.append(name.strip('\n'))
         return classes
